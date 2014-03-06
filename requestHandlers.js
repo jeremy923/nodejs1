3041facb753f1b90ac1275f8297b3fe1c911f69f
jechoi <jechoi@dhcp-40-191.bne.redhat.com>
2014-03-06 22:35:42 +1000

    handling POST

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,8 @@
 
-var exec = require("child_process").exec;
+//var exec = require("child_process").exec;
+var querystring = require('querystring');
 
-function start(response) {
+function start(response, postData) {
   console.log("** Req handler 'start' was called.");
 
   var body = '<html>'+
@@ -17,11 +18,16 @@ function start(response) {
     '</body>'+
     '</html>';
 
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
+  response.end();
+  /*
   exec("ls -alh", function (error, stdout, stderr) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write(stdout);
     response.end();
   });
+  */
 
   /*
   function sleep(ms) {
@@ -31,10 +37,11 @@ function start(response) {
   sleep(10000);
   */
 }
-function upload(response) {
+
+function upload(response, postData) {
   console.log("** Req handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " + querystring.parse(postData).text);
   response.end();
 }
 

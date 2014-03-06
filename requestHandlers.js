413251d87d2dd2468286bdf8d3b9c6a4fc9eb894
jechoi <jechoi@dhcp-40-191.bne.redhat.com>
2014-03-06 23:07:44 +1000

    almost done. somehow error occurring

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,11 @@
 
 //var exec = require("child_process").exec;
-var querystring = require('querystring');
+var querystring = require('querystring'),
+  fs = require('fs'),
+  formidable = require("formidable");
 
-function start(response, postData) {
+
+function start(response) {
   console.log("** Req handler 'start' was called.");
 
   var body = '<html>'+
@@ -11,9 +14,9 @@ function start(response, postData) {
     'charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="file" value="Upload" />'+
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -38,13 +41,44 @@ function start(response, postData) {
   */
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("** Req handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent: " + querystring.parse(postData).text);
-  response.end();
+
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, "/tmp/test.png", function(err) {
+      if (err) {
+        fs.unlink("/tmp/test.png");
+        fs.rename(files.upload.path, "/tmp/test.png");
+      }
+    });
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image: <br />");
+    response.write("<img src='/show' />");
+    response.end();
+  });
+}
+
+function show(response) {
+  console.log("** Req handler 'show' was called.");
+  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(err + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
  
